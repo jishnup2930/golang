cb3b2a9dd5e0abb1baaade1a63b14dbdee0a0dc0
jishnu <jishnup15@gmail.com>
2024-01-17 10:38:31 +0530

    Initial commit

---
+++ test.go
@@ -0,0 +1,24 @@
+package  main
+import "fmt"
+func main ()  {
+	// fmt.Println("hi")
+	var intNum int = 3215
+	intNum = intNum+1
+	fmt.Println(intNum)
+	
+	var myString  string = "It's an integer"
+	fmt.Println(myString)
+	fmt.Println(len(myString))
+
+	var myBoolean bool =false
+	fmt.Println(myBoolean)
+
+	// var myMap = map[string]uint8{"Adam":54}
+	// fmt.Println(myMap["Adam"])
+	// age,ok =["Adam":54]
+	// if ok
+	// fmt.Println("Age is %v",ok)
+	name := 62
+	fmt.Println(name)
+	
+}
